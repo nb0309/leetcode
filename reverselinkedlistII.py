3c6636f930b719ac728aeac01757b86c344cd0f2
navabhaarathi asokan <navaasokan@gmail.com>
2024-05-02 10:23:24 +0530

    making small changes in the file

--- reverselinkedlistII.py
+++ reverselinkedlistII.py
@@ -3,40 +3,35 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution:
-    def reverseBetween(self, head: [ListNode], left: int, right: int) -> [ListNode]:
-        j=1
-        rl=None
-        currentnode=head
-        l=None
-        while currentnode is not None and j!=right:
-            if j+1==left:
-                l=currentnode
-            j+=1
-            currentnode=currentnode.next
-        r=currentnode.next
-        
-        if l is not None:
-            rl=self.reverse(l.next,left,right)
-            l.next=rl
-        currentnode.next=r
-         
-        return head
-        
-        
-        
 
-    def reverse(self,head:[ListNode],value,right)-> [ListNode]:
-        j=value
-        prev=None
-        currentnode=head
-        while currentnode and j!=right:
-            j+=1
-            nextnode=currentnode.next
-            currentnode.next=prev
-            prev=currentnode
-            currentnode=nextnode
-            print(currentnode.val)
-        head=prev
-        return head
-            
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        j = 1
+        rl = None
+        currentnode = head
+        prev = None
+
+        # Move to the left position
+        while currentnode is not None and j < left:
+            prev = currentnode
+            currentnode = currentnode.next
+            j += 1
+
+        # Reverse the sublist from left to right
+        start = currentnode
+        while currentnode is not None and j <= right:
+            nextnode = currentnode.next
+            currentnode.next = rl
+            rl = currentnode
+            currentnode = nextnode
+            j += 1
+
+        # Connect the reversed sublist to the original list
+        start.next = currentnode
+
+        # If left is greater than 1, connect the previous node to the reversed sublist
+        if prev is not None:
+            prev.next = rl
+            return head
+        else:
+            return rl
